4f1e63d0b7101f3a2b82231c2f6352f93c68f367
vy538 <good5538@gmail.com>
2020-07-07 22:00:28 -0400

    updated objects

--- rct_objects.py
+++ rct_objects.py
@@ -8,14 +8,19 @@ class PeepStatus:
 		self.energy = arr[5]
 
 class Object:
-	def __init__(self,name,size,peepReactArr,time,cost,price,position=(0,0)):
+	def __init__(self,name,thirst,hunger,excitement,intensity,nausea,time,cost,price,position_x,position_y,size_x,size_y):
 		self.name = name
-		self.size = size
-		self.peepReact = Peepstatus(peepReactArr) #peep status object
-		self.time = time
-		self.buildingcost = cost
+		self.size = (size_x,size_y)
+		self.consume_time = time*size_y*size_x
+		self.building_cost = cost*size_y*size_x
 		self.price = price
-		self.position = position
+		self.position = (position_x,position_y)
+		self.excitement = excitement
+		self.intensity = intensity
+		self.nausea = nausea
+		#happiness: this is temperay measure
+		happiness = nausea*0.5+intensity*0.5
+		self.peepReact = Peepstatus([thirst,hunger,nausea,happiness,0,0]) #peep status object
 
 try:
 	fp = open('object_list.txt', 'r')
