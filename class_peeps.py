dc2c330a4a9799566494f2357aa1843060e07246
vy538 <good5538@gmail.com>
2020-07-13 17:25:59 -0400

    Create class_peeps.py
    list all the potential variable for peeps, may remove some in the future

---
+++ class_peeps.py
@@ -0,0 +1,20 @@
+import class_ride_and_store as RS
+class Peeps(self):
+    def __init__(self,intensity, nauseaTolerance):
+        self.intensity = intensity
+        self.happiness = 0
+        self.happinessTarget = 0
+        self.nausea = 0
+        self.nauseaTarget = 0
+        self.hunger = 0
+        self.nauseaTolerance = nauseaTolerance
+        self.thirst = thirst
+        self.toilet = 0
+        self.timeToConsume = 0
+        self.cash = 500
+        self.energy = 0
+        self.energyTarget = 0
+
+
+
+    def __str__(self):
