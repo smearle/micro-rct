9c81adcbad783ba4f3ef915c203c6eb83b48eb4c
vy538 <good5538@gmail.com>
2020-07-16 23:09:18 -0400

    Update rct_test_peeps.py
    test happinessUpdate

--- rct_test_peeps.py
+++ rct_test_peeps.py
@@ -1,5 +1,18 @@
+# import pygame
+import random
 import class_peeps as CP
-from rct_test_objects import object_list
+from rct_test_objects import object_list as ride
 
-pe = CP.Peeps(1)
-print(vars(pe))
+peeps = [CP.Peeps(i) for i in range(10)]
+# print(vars(peeps[0]))
+
+def main():
+    tmp2 = random.randint(0,len(ride)-7)
+    print('select ride {} intensity: {} nausea: {}'.format(ride[tmp2].name,ride[tmp2].intensity,ride[tmp2].nausea))
+    for _ in range(3):
+        tmp = random.randint(0,9)
+        print('peep: {} nauseaTolerance: {} \nbefore {}'.format(peeps[tmp].id,peeps[tmp].nauseaTolerance,peeps[tmp].happinessTarget))
+        peeps[tmp].interactWithRide(ride[1])
+        print('after {}'.format(peeps[tmp].happinessTarget))
+
+main()
