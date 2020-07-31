7c8772fc8a87fd49cb4bd60c197f8953dfad5899
vy538 <good5538@gmail.com>
2020-07-31 17:23:04 -0400

    Update rct_test_park.py

--- rct_test_park.py
+++ rct_test_park.py
@@ -3,28 +3,42 @@ from map_utility import *
 from peeps_path_finding import *
 
 def main():
-    initPark(14,14)
-    # place rides (ride object, marks, padding)
-    placeRide(ride[0],'&',1)
-    # placeRide(ride[1],'+',1)
-    # placeRide(ride[6],'A',3)
-    # place path
-    # place guest
-    guest1,guest2 = peeps[0],peeps[1]
-    updatedMap(guest1.position,(1,1),humanMark)
-    guest1.findClosesetRide(listOfRides)
-    updatedMap(guest2.position,(1,1),humanMark)
-    guest2.findClosesetRide(listOfRides)
-    
+    initPark(20,14)
+    #place the path
+    placePath(3)
     printPark()
-    for _ in range(5):
-        updatedHuman(guest1)
-        updatedHuman(guest2)
-        printPark()
+    #place the ride
+
+    #place the guest
+
+    #testing
+
+    return
     
-    for _,rid in listOfRides:
-        for peep in rid.queue:
-            print('peep {} on ride {}'.format(peep.id,rid.name))
-    return 
+
+# def main():
+#     initPark(14,14)
+#     # place rides (ride object, marks, padding)
+#     placeRide(ride[0],'&',1)
+#     # placeRide(ride[1],'+',1)
+#     # placeRide(ride[6],'A',3)
+#     # place path
+#     # place guest
+#     guest1,guest2 = peeps[0],peeps[1]
+#     updatedMap(guest1.position,(1,1),humanMark)
+#     guest1.findClosesetRide(listOfRides)
+#     updatedMap(guest2.position,(1,1),humanMark)
+#     guest2.findClosesetRide(listOfRides)
+    
+#     printPark()
+#     for _ in range(5):
+#         updatedHuman(guest1)
+#         updatedHuman(guest2)
+#         printPark()
+    
+#     for _,rid in listOfRides:
+#         for peep in rid.queue:
+#             print('peep {} on ride {}'.format(peep.id,rid.name))
+#     return 
 main()
 
