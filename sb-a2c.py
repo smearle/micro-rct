5561f5e6d540a03dfe7e38e8ca1fb96bf1a254f4
Maria <maria.edwards@gmail.com>
2020-11-01 16:35:01 -0500

    adding training and logging for simple A2C model

--- sb-a2c.py
+++ sb-a2c.py
@@ -1,10 +1,80 @@
 import gym
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 from gym_micro_rct.envs.rct_env import RCT
+
 from stable_baselines3 import A2C
 from stable_baselines3.a2c import MlpPolicy
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.env_util import make_vec_env
 
-env = RCT(settings_path='configs/settings.yml')
 
-model = A2C(MlpPolicy, env)
-model.learn(total_timesteps=5000)
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print(f"Saving new best model to {self.save_path}.zip")
+                  self.model.save(self.save_path)
+
+        return True
+
+
+
+log_dir = "./tmp/a2c-log"
+os.makedirs(log_dir, exist_ok=True)
+
+tb_logs = "./tmp/a2c-tb-log"
+os.makedirs(tb_logs, exist_ok=True)
+
+env = RCT(settings_path='configs/settings.yml')
+env = Monitor(env, log_dir)
+
+n_actions = env.action_space.shape[-1]
+#action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+
+model = A2C(MlpPolicy, env, tensorboard_log = tb_logs, verbose=0)
+model.learn(total_timesteps=int(5e4), callback=callback)
+
+results_plotter.plot_results([log_dir], 1e5, results_plotter.X_TIMESTEPS, "A2C MlpPolicy RCT")
