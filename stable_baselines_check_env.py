3c20e675d821e837328b7b1b5d848e93674d94b0
Sam Earle <smearle93@gmail.com>
2020-11-15 15:03:45 -0500

    Remove some RL scaffolding before merge w/ master.

--- stable_baselines_check_env.py
+++
@@ -1,7 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from gym_micro_rct.envs.rct_env import RCT
-
-env = RCT(settings_path='configs/settings.yml')
-
-# It will check your custom environment and output additional warnings if needed
-check_env(env, warn=True)
