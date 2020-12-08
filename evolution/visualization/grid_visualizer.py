0a7dbdddb3b07d9f912aefff29c5a497c4d40625
mcgreentn <mcg520@nyu.edu>
2020-12-08 10:58:45 -0600

    made some changes to visualization

--- evolution/visualization/grid_visualizer.py
+++ evolution/visualization/grid_visualizer.py
@@ -1,4 +1,6 @@
 import plotly.express as px
+import plotly.graph_objects as go
+
 import pandas as pd
 import numpy as np
 class GridVisualizer:
@@ -9,21 +11,29 @@ class GridVisualizer:
     def visualize(self, x, y, val, write_path=None, x_skip=1, y_skip=1):
         temp = self.df.pivot(index=y, columns=x)[val]
         # temp = temp.fillna(-10)
-        temp = temp.reindex(list(range(temp.index.min(), temp.index.max()+1, y_skip)), fill_value=np.nan)
+        temp = temp.reindex(list(range(0, temp.index.max()+1, y_skip)), fill_value=np.nan)
         #temporary transpose to fill in missing column values
         temp = temp.T
-        temp = temp.reindex(list(range(temp.index.min(), temp.index.max()+1, x_skip)), fill_value=np.nan)
+        temp = temp.reindex(list(range(0, temp.index.max()+1, x_skip)), fill_value=np.nan)
         temp = temp.T
-        fig = px.imshow(temp)
-        fig['layout'].update(plot_bgcolor='rgb(203, 213, 232)')
+        # fig = px.imshow(temp)
+        fig = go.Figure(data=go.Heatmap(
+            z=temp.values.tolist(),
+            x=temp.columns.tolist(),
+            y=temp.index.tolist(),
+            colorscale='Viridis'))
+        # fig['layout'].update(plot_bgcolor='rgb(203, 213, 232)')
         fig['layout'].update(
             xaxis=dict(
-                tickmode='linear',
-                tick0=0,
-                dtick=x_skip),
-            yaxis=dict(tickmode='linear',
-                tick0=0,
-                dtick=y_skip)
+                # tickmode='linear',
+                # tick0=0,
+                # dtick=x_skip,
+                showgrid=False),
+            yaxis=dict(
+                # tickmode='linear',
+                # tick0=0,
+                # dtick=y_skip,
+                showgrid=False)
         )
 
         fig['layout']['yaxis']['autorange'] = "reversed"
